2fa7422f9e54eb8a69034dd4fa6afbc99e99e339
Timothy <timothymathew2011@gmail.com>
2025-02-17 18:56:37 +0400

    Fix method for /zoom_webhook

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     return "Zoom Bot is Running!"
 
 # Ensure this route accepts POST requests
-@app.route("/zoom_webhook", methods=["GET"])
+@app.route("/zoom_webhook", methods=["POST"])
 def zoom_webhook():
     data = request.json
 
